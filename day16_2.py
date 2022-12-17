0e3ef0d573697413c1f000d095c992eb553ce86b
ThatScar <neskalmantas@gmail.com>
2022-12-17 14:01:20 +0200

    Add files via upload

--- day16_2.py
+++ day16_2.py
@@ -1,3 +1,5 @@
+import time
+
 class Valve:
     def __init__(self, name, rate, tunnels):
         self.name = name
@@ -12,7 +14,7 @@ zero, not_zero = [], []
 with open("day16.txt") as file:
     for line in file:
         line = "".join([x for x in line if not x.islower() and not x.isspace()])
-        print(line)
+##        print(line)
         name = line[1:3]
         rate, tunnels = line[4:].split(";")
         rate = int(rate)
@@ -36,24 +38,34 @@ for valve in not_zero + [valves["AA"]]:
     for other in set(zero + [valve]):
         valve.distances.pop(other.name)
 
+start_time = time.time()
+
 high_score = 0
-def dfs(remaining, score, visited, last, stack):
+def dfs(remaining, remaining2, score, visited, last, last2, stack, stack2):
     global high_score
+    if remaining < remaining2:
+        remaining, remaining2 = remaining2, remaining
+        last, last2 = last2, last
+        stack, stack2 = stack2, stack
     if remaining < 0:
         return
     score += remaining * last.rate
     if high_score < score:
         high_score = score
-        print(high_score)
-        print(stack)
-    for name, distance in last.distances.items():
+##        print(score)
+##        print(stack)
+##        print(stack2[:-1])
+##        print(f"{time.time() - start_time} seconds in")
+    for name, distance in list(last.distances.items()) + [("AA", 123456789)]:
         if name not in visited:
             cost = distance + 1
             valve = valves[name]
             visited.add(name)
             stack.append(name)
-            dfs(remaining-cost, score, visited, valve, stack)
+            dfs(remaining-cost, remaining2, score, visited, valve, last2, stack, stack2)
             stack.pop()
             visited.remove(name)
 
-dfs(30, 0, set(), valves["AA"], [])
+dfs(26, 26, 0, set(), valves["AA"], valves["AA"], [], [])
+print(high_score)
+print("--- %s seconds ---" % (time.time() - start_time))
